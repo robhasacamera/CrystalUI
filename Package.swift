27c94fc21f1b1b11b5e11496e615e00093e5da1c
Robert Cole <robhasacamera@gmail.com>
2022-09-03 12:05:10 -0700

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CrystalUI",
+    platforms: [.iOS(.v15), .macOS(.v12)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "CrystalUI",
+            targets: ["CrystalUI"]),
+    ],
+    dependencies: [
+        .package(
+            name: "CUIExpandableButton",
+            url: "https://github.com/robhasacamera/CUIExpandableButton.git",
+            from: "0.13.0"
+        )
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "CrystalUI",
+            dependencies: ["CUIExpandableButton"]),
+        .testTarget(
+            name: "CrystalUITests",
+            dependencies: ["CrystalUI"]),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,19 @@
+# Crystal UI
+
+Crystal UI is a collection of SwiftUI Views and utilities meant to give a cohesive yet customizable UI for any app.
+
+## Installation
+
+Crystal UI supports Swift Package Manager. To use it the following to your `Package.swift` file:
+
+```
+dependencies: [
+    .package(name: "CUIExpandableButton", url: "https://github.com/robhasacamera/CUIExpandableButton.git", from: "0.1.0")
+],
+```
+
+## Future Plans
+
+1. Add addition views and utilities
+2. Add more comprehensive documention
+3. Create a documentation portal
